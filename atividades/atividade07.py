4e78d8d7b749bc080c034423838a6a0396551ebd
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:49:42 -0300

    feat: adiciona a atividade07

---
+++ atividades/atividade07.py
@@ -0,0 +1,20 @@
+# escreva("Nota 1: ")
+# leia(n1)
+n1 = float(input("Nota 1: "))
+# escreva("Nota 2: ")
+# leia(n2)
+n2 = float(input("Nota 2: "))
+# escreva("Nota 3: ")
+# leia(n3)
+n3 = float(input("Nota 3: "))
+# media <- (n1 + n2 + n3) / 3
+media = (n1 + n2 + n3) / 3
+# se media >= 7 entao
+if media >= 7:
+    # escreva("APROVADO!")
+    print('APROVADO!')
+# senao
+else:
+    # escreva("ops! REPROVADO!!")
+    print('ops! REPROVADO!!')
+# fimse
