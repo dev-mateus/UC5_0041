bcc4a011e25d17e79ba468ca14a7b68412cda3ae
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:50:11 -0300

    feat: adiciona a atividade10

---
+++ atividades/atividade10.py
@@ -0,0 +1,15 @@
+# escreva("Digite um numero: ")
+# leia(numero1)
+numero1 = int(input('Digite um número: '))
+# escreva("Digite um numero: ")
+# leia(numer2)
+numero2 = int(input('Digite um número: '))
+# se (numero1 = numero2) entao
+if numero1 == numero2:
+    # escreva("Esses números são iguais!")
+    print("Esses números são iguais!")
+# senao
+else:
+    # escreva("Esses números são diferentes!")
+    print("Esses números são diferentes!")
+# fimse
