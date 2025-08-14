ed87c3236f875158d78e660fa3cea78f71465714
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:48:56 -0300

    feat: adiciona a atividade03

---
+++ atividades/atividade03.py
@@ -0,0 +1,15 @@
+# c <- 3
+c = 3
+# escreva("Nota 1: ")
+# leia(n1)
+n1 = float(input("Nota 1: "))
+# escreva("Nota 2: ")
+# leia(n2)
+n2 = float(input("Nota 2: "))
+# escreva("Nota 3: ")
+# leia(n3)
+n3 = float(input("Nota 3: "))
+# media <- (n1 + n2 + n3) / c
+media = (n1 + n2 + n3)/3
+# escreva("A média das notas é:", media)
+print("A média das notas é:", media)
