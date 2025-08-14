d906ab74ef1bdf7e63f3af93d616d4523c495a64
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:48:44 -0300

    feat: adiciona a atividade02

---
+++ atividades/atividade02.py
@@ -0,0 +1,11 @@
+# c1 <- 1.8
+c1 = 1.8
+# c2 <- 32
+c2 = 32
+# escreva("Temperatura ºC: ")
+# leia(tc)
+tc = float(input("Temperatura ºC: "))
+# tf <- tc * c1 + c2
+tf = tc * c1 + c2
+# escreva("Temperatura F:", tf)
+print("Temperatura F:", tf)
