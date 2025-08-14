c0b9f6f28da290f5bde3268541bff0c862ce554f
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:50:03 -0300

    feat: adiciona a atividade09

---
+++ atividades/atividade09.py
@@ -0,0 +1,11 @@
+# escreva("Digite um numero: ")
+# leia(numero)
+numero = int(input('Digite um numero: '))
+# se (numero mod 2 = 0) entao
+if numero % 2 == 0:
+    # escreva(numero," é PAR")
+    print(f'{numero} é PAR')
+else:
+    # escreva(numero," é IMPAR")
+    print(f'{numero} é IMPAR')
+# fimse
