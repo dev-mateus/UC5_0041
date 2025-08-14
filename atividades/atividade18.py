dc9ff46b73a498093b3038d6dc5be321ab4b3376
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:51:15 -0300

    feat: adiciona a atividade18

---
+++ atividades/atividade18.py
@@ -0,0 +1,10 @@
+# escreva("Informe um numero: ")
+# leia(numero)
+numero = int(input('Informe um numero: '))
+# para i de 1 ate 10 faca
+for i in range(1, 11):
+    # resultado <- numero * i
+    resultado = numero * i
+    # escreval(numero, " x", i, " =", resultado)
+    print(numero, "x", i, "=", resultado)
+# fimpara
