968e6f0ec4db03586b5f107bf5d18ea2089b9529
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:51:00 -0300

    feat: adiciona a atividade16

---
+++ atividades/atividade16.py
@@ -0,0 +1,16 @@
+# numero <- 30
+numero = 30
+# enquanto numero > 0 faca
+while numero > 0:
+    # se (numero mod 4 = 0) entao
+    if numero % 4 == 0:
+        # escreva(" [", numero, " ]")
+        print("[", numero, "]")
+    # senao
+    else:
+        # escreva(numero)
+        print(numero)
+    # fimse
+    # numero <- numero - 1
+    numero -= 1
+# fimenquanto
