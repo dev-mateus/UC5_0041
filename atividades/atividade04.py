79619c092db9f6ad159031e45a5ea7640fda8d5e
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:49:09 -0300

    feat: adiciona a atividade04

---
+++ atividades/atividade04.py
@@ -0,0 +1,13 @@
+# c <- 2
+c = 2
+# escreva("Digite um número inteiro: ")
+# leia(numero)
+numero = int(input("Digite um número inteiro: "))
+# dobro <- numero * c
+dobro = numero * c
+# metade <- numero / c
+metade = numero / c
+# escreval("O dobro é:", dobro)
+print("O dobro é:", dobro)
+# escreva("A metade é:", metade)
+print("A metade é:", metade)
