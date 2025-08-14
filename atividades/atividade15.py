313058c175f36b4f4fba0e16371099de6bc5fc99
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:50:52 -0300

    feat: adiciona a atividade15

---
+++ atividades/atividade15.py
@@ -0,0 +1,12 @@
+# escreva("Informe um número inteiro positivo: ")
+# leia(numero)
+numero = int(input("Informe um número inteiro positivo: "))
+# contador <- 1
+contador = 1
+# enquanto contador <= numero faca
+while contador <= numero:
+    # escreval(contador)
+    print(contador)
+    # contador <- contador + 2
+    contador += 2
+# fimenquanto
