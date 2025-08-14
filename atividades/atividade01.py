52deba9791ce5d95d25bc14b1f78e3d72b1c8d1d
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:47:12 -0300

    feat: adiciona a atividade01

---
+++ atividades/atividade01.py
@@ -0,0 +1,12 @@
+# escreva("base: ")
+print("base: ")
+# leia(base)
+base = float(input())
+# escreva("altura: ")
+print("altura: ")
+# leia(altura)
+altura = float(input())
+# area <- base * altura
+area = base * altura
+# escreva("A area é: ", area)
+print("A area é: ", area)
