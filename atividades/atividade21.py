82383e19ed7c0ba6ae27b97e4d3d80309145069e
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:51:33 -0300

    feat: adiciona a atividade21

---
+++ atividades/atividade21.py
@@ -0,0 +1,12 @@
+# soma <- 0
+soma = 0
+# para controle de 1 ate 7 faca
+for controle in range(1, 8):
+    # escreva("Digite um numero: ")
+    # leia(numero)
+    numero = float(input("Digite um numero: "))
+    # soma <- soma + numero
+    soma = soma + numero
+# fimpara
+# escreva("A soma de todos os numeros é:", soma)
+print("A soma de todos os numeros é:", soma)
