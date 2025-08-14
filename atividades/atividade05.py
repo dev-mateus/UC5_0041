cdfd9de482dc977d8d30a35a52b8a2582751615d
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-14 16:49:24 -0300

    feat: adiciona a atividade05

---
+++ atividades/atividade05.py
@@ -0,0 +1,9 @@
+# desconto <- 0.9
+desconto = 0.9
+# escreva("Qual o valor do produto? R$")
+# leia(valorOriginal)
+valorOriginal = float(input("Qual o valor do produto? R$"))
+# valorFinal <- valorOriginal * desconto
+valorFinal = valorOriginal * desconto
+# escreva("O valor do produto com desconto é R$", valorFinal)
+print("O valor do produto com desconto é R$", valorFinal)
