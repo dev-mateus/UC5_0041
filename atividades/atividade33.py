c2b7802e2542bec772588e138104e3dc964359ee
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-28 17:11:47 -0300

    feat: adiciona a atividade33

---
+++ atividades/atividade33.py
@@ -0,0 +1,17 @@
+def calcula_imc(peso, altura):
+    imc = peso / altura ** 2
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif imc < 25:
+        return "Peso normal"
+    elif imc < 30:
+        return "Sobrepeso"
+    elif imc < 35:
+        return "Obesidade grau I"
+    elif imc < 40:
+        return "Obesidade grau II"
+    else:
+        return "Obesidade grau III"
+
+
+calcula_imc(55, 1.56)
