a8a36ceab5832cd43d4b9f6b617bc941e0ad8405
Mateus Pereira <mateus.pereira4@gmail.com>
2025-08-28 17:10:05 -0300

    feat: adiciona a atividade27-1

---
+++ atividades/atividade27-1.py
@@ -0,0 +1,15 @@
+# para a de 0 ate 9 faca
+#      escreva("Informe um numero real: ")
+#      leia(numeros[a])
+# fimpara
+#
+# para a de 0 ate 9 faca
+#      soma <- soma + numeros[a]
+# fimpara
+# media <- soma / 10
+#
+# para a de 0 ate 9 faca
+#      se (numeros[a] > media) entao
+#         escreval(numeros[a])
+#      fimse
+# fimpara
