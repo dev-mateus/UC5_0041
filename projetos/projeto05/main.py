b142e0920c5432bedd95e7d42c6af7910dc13974
dev-mateus <mateus.pereira4@gmail.com>
2025-10-09 15:38:48 -0300

    feat: adiciona o projeto05

---
+++ projetos/projeto05/main.py
@@ -0,0 +1,27 @@
+import customtkinter
+
+
+class Janela:
+    def __init__(self):
+        self.janela = customtkinter.CTk()
+        self.janela.title
+        self.janela.title("Minha Janela")
+        self.janela.geometry("400x300")
+
+        self.entrada = customtkinter.CTkEntry(self.janela, placeholder_text="Digite algo aqui")
+        self.entrada.pack(pady=20)
+
+        self.botao = customtkinter.CTkButton(self.janela, text="Clique aqui", command=self.botao_clicado)
+        self.botao.pack(pady=10)
+
+        self.saida = customtkinter.CTkLabel(self.janela, text="Saída aparecerá aqui")
+        self.saida.pack(pady=20)
+
+        self.janela.mainloop()
+
+    def botao_clicado(self):
+        texto = self.entrada.get()
+        self.saida.configure(text=texto)
+
+if __name__ == "__main__":
+    Janela()
